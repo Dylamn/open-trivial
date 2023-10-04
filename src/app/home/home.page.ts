954d3d6710bf059e271b478bcfba1e642881a5f0
Dylamn <dylan.vallee@hotmail.com>
2023-10-04 15:48:52 +0200

    feat: add remember me implementation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import type { Difficulties, Question } from 'src/types'
-import { OpenTriviaService } from '../services/open-trivia.service'
+import { Preferences } from '@capacitor/preferences'
 import { Router } from '@angular/router'
 
 @Component({
@@ -8,7 +8,7 @@ import { Router } from '@angular/router'
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   constructor(private router: Router) {}
 
   public alertButtons = ['OK']
@@ -17,25 +17,64 @@ export class HomePage {
   username?: string
   errorMsg: string | null = null
   rememberMe: boolean = false
+  preferencesLoaded: boolean = false
 
   selectedDifficulty: Difficulties = 'easy'
   difficulties: Array<Difficulties> = ['easy', 'medium', 'hard']
+
+  async ngOnInit() {
+    const { value: username } = await Preferences.get({ key: 'username' })
+    this.username = username || ""
+
+    const { value: difficulty } = await Preferences.get({ key: 'difficulty' })
+    this.selectedDifficulty = (difficulty as Difficulties) || 'easy'
+    
+    this.rememberMe = username !== null && difficulty !== null
+    this.preferencesLoaded = this.rememberMe
+  }
 
   setErrorAlert(open: boolean) {
     this.displayError = open
   }
 
-  startGame() {
-    const length = this.username?.length || 0
-
-    if (length < 3) {
+  async startGame() {
+    if (!this.validateUsername()) {
       this.errorMsg = 'Le pseudonyme doit contenir au moins 3 caractères.'
       this.setErrorAlert(true)
       return
     }
 
+    if (this.rememberMe) {
+      console.info("remember :: save preferences")
+      await this.savePreferences()
+    } else if (this.preferencesLoaded) { // rememberMe is false here
+      this.clearPreferences()
+    }
+
     this.router.navigate(['/game'], {
       queryParams: { username: this.username, difficulty: this.selectedDifficulty },
     })
+  }
+
+  async savePreferences() {
+    await Preferences.set({
+      key: 'username',
+      value: this.username || ''
+    })
+
+    await Preferences.set({
+      key: 'difficulty',
+      value: this.selectedDifficulty
+    })
+  }
+
+  async clearPreferences() {
+    await Preferences.clear()
+  }
+
+  validateUsername() {
+    const length = this.username?.length || 0
+
+    return length >= 3
   }
 }
--- src/app/score/score.page.ts
+++ src/app/score/score.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { NavController } from '@ionic/angular'
 import { DataService } from '../services/data.service'
+import { Camera, CameraResultType } from '@capacitor/camera'
 
 @Component({
   selector: 'app-score',
@@ -10,7 +11,7 @@ import { DataService } from '../services/data.service'
 })
 export class ScorePage implements OnInit {
   points: number = 0
-
+  photo: string | undefined
   constructor(private nav: NavController, private activatedRoute: ActivatedRoute, private dataSrv: DataService) {}
 
   ngOnInit() {
@@ -19,5 +20,19 @@ export class ScorePage implements OnInit {
 
   restart() {
     this.nav.navigateRoot(['/home'])
+  }
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+    })
+    
+    if (!image?.webPath) {
+      return
+    }
+    
+    this.photo = image.webPath
   }
 }
